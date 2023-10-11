3630c33ade35d05568ff79ea8f8b7106b6142385
Ethan Tang <itsethang5@gmail.com>
2023-10-11 00:51:17 -0400

    init responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Welcome!</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"filter slide"
+        "main slide"`,
+        lg: `"header"
+        "filter"
+        "main"`,
+      }}
+    >
+      <Show above="lg">
+        <GridItem bg="blue.300" area={"header"}>
+          Header
+        </GridItem>
+      </Show>
+      <GridItem bg="red.300" area={"filter"}>
+        Filter
+      </GridItem>
+      <GridItem bg="yellow.300" area={"main"}>
+        Main
+      </GridItem>
+      <Show below="lg">
+        <GridItem bg="pink.300" area={"slide"}>
+          Slide
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
